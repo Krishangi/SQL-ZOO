8626c421d6fa91979cbb07a8bccbd43d49b01bf1
Krishangi Goswami <56845004+Krishangi@users.noreply.github.com>
2021-01-26 21:16:57 -0500

    Update more-join.sql

--- more-join.sql
+++ more-join.sql
@@ -1,3 +1,4 @@
+#TABLE RELATIONSHIP - https://sqlzoo.net/wiki/File:Movie2-er.png
 #TABLE 1 -> movie (id, title,	yr,	director,	budget,	gross )
 #TABLE 2 -> actor (id,	name )
 #TABLE 3 -> casting (movieid,	actorid,	ord)
@@ -79,7 +80,30 @@ WHERE actorid = (SELECT id
                   WHERE name='Harrison Ford') 
 AND ord <> 1
 
-#10. 
+#10. List the films together with the leading star for all 1962 films.
 
-                 
+SELECT movie.title, actor.name
+FROM movie
+JOIN casting ON movie.id=casting.movieid
+JOIN actor ON actor.id=casting.actorid
+WHERE movie.yr=1962 
+AND casting.ord=1
+
+#11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+
+SELECT movie.yr, COUNT(DISTINCT movie.id) AS 'No. of movies'
+FROM movie
+JOIN casting ON casting.movieid=movie.id
+JOIN actor ON actor.id = casting.actorid
+WHERE name='Rock Hudson'
+GROUP BY movie.yr
+HAVING COUNT(DISTINCT movie.id) > 2
+
+#12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+#13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+
+#14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
+#15. List all the people who have worked with 'Art Garfunkel'.
 
