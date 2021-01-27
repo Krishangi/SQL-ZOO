1759cb5fe29c9718fd31f6567e33c0dfd396eb86
Krishangi Goswami <56845004+Krishangi@users.noreply.github.com>
2021-01-26 21:20:45 -0500

    Create using-null.sql

---
+++ using-null.sql
@@ -0,0 +1,9 @@
+## TABLE 1 ->> teacher ( id,	dept,	name,	phone,	mobile )
+## TABLE 2 ->> dept (id,	name )
+## The school includes many departments. Most teachers work exclusively for a single department. Some teachers have no department. ##
+
+
+#1. List the teachers who have NULL for their department.
+
+
+
